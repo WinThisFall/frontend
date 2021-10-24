b561e5c99450b76cd7235dd531a1f739e297d5ec
janaSunrise <warriordefenderz@gmail.com>
2021-10-24 09:59:22 +0530

    Redesign the UI

--- pages/add.jsx
+++ pages/add.jsx
@@ -1,4 +1,17 @@
-import { Flex, Box, Input, Button } from '@chakra-ui/react';
+import {
+  Flex,
+  Box,
+  Input,
+  Button,
+  FormControl,
+  FormLabel,
+  Checkbox,
+  Stack,
+  Link,
+  Heading,
+  Text,
+  useColorModeValue
+} from '@chakra-ui/react';
 import { useState } from 'react';
 import { create, urlSource } from 'ipfs-http-client';
 
@@ -28,16 +41,47 @@ const Add = () => {
   };
 
   return (
-    <Box minHeight="10">
-      <Flex justifyContent={'center'} alignItems={'center'} dir={'column'}>
-        <Input
-          marginTop="5"
-          type="file"
-          onChange={e => setFile(e.target.files[0])}
-        />
-        <Button onClick={addFile}>Upload</Button>
-      </Flex>
-    </Box>
+    <Flex
+      minH={'100vh'}
+      align={'center'}
+      justify={'center'}
+      bg={useColorModeValue('gray.50', 'gray.800')}
+    >
+      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+        <Stack align={'center'}>
+          <Heading fontSize={'4xl'} fontFamily="Poppins">
+            Upload your file 📂
+          </Heading>
+        </Stack>
+        <Box
+          rounded={'lg'}
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow={'lg'}
+          p={8}
+        >
+          <Stack spacing={4}>
+            <FormControl id="file">
+              <FormLabel fontFamily="Poppins" textAlign="center">Upload your file here!</FormLabel>
+              <Input type="file" onChange={e => setFile(e.target.files[0])} />
+            </FormControl>
+            <Stack spacing={10}>
+              <Button
+                bg={'blue.400'}
+                color={'white'}
+                _hover={{
+                  bg: 'blue.500'
+                }}
+                onClick={addFile}
+                fontFamily="Poppins"
+                variant="outline"
+              >
+                Upload
+              </Button>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
   );
 };
 
